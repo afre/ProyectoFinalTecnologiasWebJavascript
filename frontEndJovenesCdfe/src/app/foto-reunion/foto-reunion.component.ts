08bc02fb883c42c21c1be9878ba5cb7e7032bf46
David Moncayo <davidmoncayo4@gmail.com>
2017-03-08 10:50:40 -0500

    Sin probar los modulos, puede que ya funcione :see_no_evil:
    :dancer: :see_no_evil:

--- frontEndJovenesCdfe/src/app/foto-reunion/foto-reunion.component.ts
+++ frontEndJovenesCdfe/src/app/foto-reunion/foto-reunion.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Http, Response} from "@angular/http";
+import {MasterUrlService} from "../services/master-url.service";
+import {NgForm} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-foto-reunion',
@@ -7,9 +11,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FotoReunionComponent implements OnInit {
 
-  constructor() { }
+  private _parametros: any;
+  fotosReunion=[];
+  nuevaFotoReunion={};
+
+  constructor(private _ActivedRoute: ActivatedRoute, private _http:Http,private _masterURL:MasterUrlService) { }
 
   ngOnInit() {
+
+    this.listarFotoReunion();
+
+  }
+
+
+
+  crearFotoReunion(formulario: NgForm) {
+
+
+    this._http.post(this._masterURL.url + "FotoReunion", {
+      archivo_Foto: formulario.value.archivo_Foto,
+      idReunionJovenes:this._parametros.idReunionJovenes
+    }).subscribe(
+      (res:Response) => {
+
+        this.fotosReunion.push(res.json());
+        this.fotosReunion[this.fotosReunion.length-1].formularioCerrado=true;
+        this.nuevaFotoReunion = {};
+
+      },
+      (err) => {
+        console.log("Ocurrio un error", err);
+      }
+    );
+
+
+  }
+
+  listarFotoReunion(){
+    this._ActivedRoute.params.subscribe(
+      parametros=>{
+        this._parametros=parametros;
+        this._http.get(this._masterURL.url+'FotoReunion?idReunionJovenes='+this._parametros.idReunionJovenes)
+          .subscribe(
+            (res:Response)=>{
+              this.fotosReunion=res.json().map((value)=>{
+                value.formularioCerrado=true;
+                return value;
+              });
+
+            },
+            (err) => {
+              console.log("Ocurrio un error", err);
+            }
+          );
+      }
+    )
+
+
+  }
+
+  borrarFotoReunion(id: number) {
+    this._http.delete(this._masterURL.url + "FotoReunion/" + id)
+      .subscribe(
+        (res) => {
+          let predicaFotoReunionBorrada = res.json();
+          this.fotosReunion = this.fotosReunion.filter(value => predicaFotoReunionBorrada.id != value.id);
+        },
+        (err) => {
+          console.log(err);
+        }
+      )
+  }
+
+  editarFotoReunion(fotoReunion: any) {
+    let parametros = {
+      archivo_Foto: fotoReunion.archivo_Foto,
+      idReunionJovenes:this._parametros.idReunionJovenes
+    };
+    this._http.put(this._masterURL.url + "FotoReunion/" + fotoReunion.id, parametros)
+      .subscribe(
+        (res: Response) => {
+          fotoReunion.formularioCerrado = !fotoReunion.formularioCerrado;
+
+        },
+        (err) => {
+          console.log("Error:", err);
+        }
+      )
   }
 
 }
